21fb7a229cd66275dd963b2dca894a0dd424318f
nathanmega <124056149+nathanmega@users.noreply.github.com>
2023-02-01 16:54:45 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body style="background-color:white">
+    <div class="col-md-4"></div>
+    <div class="col-md-4" style="background-color:blue">
+        <form class="formone" method="post" style="height:403px">
+            <h1 class="" align="center">Book a call</h1>
+            <hr>
+            <label name="NAME" align="left">NAME</label>
+            <input type="text" class="form-control" id="name" name=""><br>
+            <label name="EMAIL" align="left">EMAIL</label><br>
+            <input type="text" id="EMAIL" class="form-control"><br>
+            <label name="PHONE_NO"  align=" left">PHONE NO</label><br>
+            <input type="text" id="phone_no" name="" class="form-control"><br>
+            <button class="btn btn-primary value="">getacall</button>
+      
+        </form>
+    </div>
+    <label for="nm" value="one"></label>
+    <label for="em" value="two"></label>
+    <label for="ph" value="three"></label>
+    <br>
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+
+   
+ 
+
+        <script src="global.js"> </script>
+        <script>
+            const form_1 = document.querySelector("formone");
+    const namenx =document.querySelector("name");
+    alert("namenx");
+        </script>
+
+     
+
+
+
+
+</body>
+
+</html>
