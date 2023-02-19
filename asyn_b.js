9545b78371ed864a09e2b9839f252271918d3e05
nathanmega <124056149+nathanmega@users.noreply.github.com>
2023-02-19 16:02:23 +0530

    Update asyn_b.js

--- asyn_b.js
+++ asyn_b.js
@@ -33,3 +33,28 @@ const preMovie = async () => {
 preMovie().then((t) => console.log(`person4 shows ${t}`));
 
 console.log('person4 shows ticket');
+
+
+
+const preMovie = async () => {
+
+  const person3PromiseToShowTicketWhenWifeArrives = new Promise((resolve, reject) => {
+    setTimeout(() => reject('ticket'), 3000);
+  });
+
+  let ticket;
+	try {
+  	ticket = await person3PromiseToShowTicketWhenWifeArrives;
+  } catch(e) {
+  	ticket = 'sad face';
+  }
+  
+
+  return ticket;
+  
+};
+
+preMovie().then((t) => console.log(`person4 shows ${t}`));
+
+
+
